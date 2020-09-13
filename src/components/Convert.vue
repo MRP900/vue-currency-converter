d967ad7fc758ffe1a8dac7559eab7391c7df44f8
Matt <mattrpittman@gmail.com>
2020-09-13 12:43:26 -0600

    "rm files, before adding additional selectors"

--- src/components/Convert.vue
+++ src/components/Convert.vue
@@ -1,53 +1,76 @@
 <template>
     <div>
-        <form @submit="convertCurrency">
+        <form @submit="convert">
             <fieldset>
+                <legend>Dollars to ...</legend>
                 <div id="fields">
-                    <input type="text" v-model="money" name="money" placeholder="Amount to convert"><br>
-                    <input type="text" v-model="convertFrom" name="convertFrom" placeholder="Convert from - country"><br>
-                    <input type="text" v-model="convertTo" name="convertTo" placeholder="Covert to - country"><br>
+                    <input type="text" v-model="money" name="money" placeholder="Amount to Convert"><br>
+                        <div id="selectors">
+                            <select id="dropDown1" v-model="convertFrom">
+                                <option value="Convert From" selected>Convert From</option>
+                                <option v-for="rate in rates" v-bind:key="rate.id">
+                                    {{ rate.abb }}
+                                </option> 
+                            </select>
+                            
+                            <select id="dropDown2" v-model="selected">
+                                <option value="Convert to" selected>Convert to</option>
+                                <option v-for="rate in rates" v-bind:key="rate.id">
+                                    {{ rate.abb }}
+                                </option> 
+                            </select>
+                        </div>
                 </div>
                 <div id="buttons">
                     <input type ="submit" value="Convert" class="btn">
-                    <input type ="reset" value="Reset" class="btn">
+                    <input type ="reset" @click="clearOut" value="Reset" class="btn">
                 </div>
             </fieldset>
             
         </form>
     </div>
+    
+    
 </template>
 
 <script>
 
 export default {
     name: "Convert",
-    data() {
+    props: ["rates"],
+    data: ()=> {
         return {
-            money : '',
-            convertFrom : '',
-            convertTo : '',
+            money : "",
+            convertFrom: "Convert From",
+            selected : "Convert to"
         }
     },
     methods: {
-        convertCurrency(e) {
+        convert(e) {
             e.preventDefault();
-            const newConversion = {
-                money : this.money,
-                cFrom : this.convertFrom,
-                cTo : this.convertTo
+            if (!isNaN(this.money) && (this.money.length > 0) && (this.selected != "Convert to")) {
+                // const money = this.money;
+                // const rate = this.selected;
+                console.log("Hello, from convert");
+                console.log(this.selected);
+                console.log(this.money);
+                this.$emit('new-convert', this.money, this.selected);
             }
-            // Send to parent
-            this.$emit('new-conversion', newConversion);
-
-            // money : '';
-            // convertFrom : '';
-            // convertTo : '';
+        },
+        clearOut() {
+            this.money = "";
+            this.$emit('clear-out');
         }
     } 
 }
 </script>
 
 <style scoped>
+    legend {
+        font-style: italic;
+        font-weight: bold;
+        text-align: left;
+    }
     form {
         width: 40%;
         margin: 2.5em auto;
@@ -62,5 +85,12 @@ export default {
     }
     #buttons input {
         margin-right: .4em;
+    }
+    #selectors {
+        width: 90%;
+        margin: .5em auto;
+    }
+    #dropDown1 {
+        margin-right: 1em;
     }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,92 @@
 <template>
-  <div id="app">
-    <Header />
-    <Convert />
-  </div>
+	<div id="app">
+		<Header />
+		<Convert v-on:new-convert="convertUSD" v-on:clear-out="clearOut" v-bind:rates="this.cleanData" />
+		<!-- <Result v-bind:message2="output" /> -->
+		<!-- <div v-if="converted.length > 0">
+			<h3>Dollar to Euro</h3>
+			<h3>{{ apiResult.rates.USD }} : 1</h3>
+			<h3>Dollars to {{ rate }} {{ converted }}</h3>
+		</div> -->
+		<h3>{{ converted }}</h3>
+	</div>
 </template>
 
 <script>
-// import axios from 'axios';
+import axios from 'axios';
 import Convert from '../components/Convert';
 
+
 export default {
-  name: 'Home',
-  components: {
-    Convert
-  }
+	name: 'Home',
+	components: {
+		Convert
+	},
+	methods: {
+		convertUSD(money, rate) {		
+			this.converted = money / this.apiResult.rates.USD;
+			this.rate = rate;
+			if (this.rate != 'EUR') {
+				this.converted *= this.apiResult.rates[this.rate];
+			}
+			// // this.rate = rate;
+			// if (rate != 'EUR') {
+			// 	this.converted = this.converted / this.apiResult.rates[rate];
+			// }
+			console.log(this.apiResult.rates.EUR);
+			console.log(this.converted);
+		},
+		clearOut() {
+			this.converted = "";
+		}
+		
+	},
+	data() {
+		return {
+			messages: {
+				m1: 'Enter a Dollar amount'
+			},
+			apiResult: {
+
+			},
+			// euros: "",
+			rate: "",
+			converted : "",
+			abb : [],
+			rates : [],
+			cleanData : []
+		}
+	},
+	created() {
+		axios.get('http://data.fixer.io/api/latest?access_key=893d6c36e2e81633bee78cd279acba84').then(res => {
+		// console.log(res.data);
+		// console.log(this.apiResult.rates.USD);
+		// this.usdRate = this.apiResult.rates.USD;
+		this.apiResult = res.data;
+		this.abb = Object.keys(this.apiResult.rates);
+		this.rates = Object.values(this.apiResult.rates);
+		// // this.usdRate = this.apiResult.rates.USD;
+		// this.keys = this.apiResult.rates;
+		for (let i = 0; i < this.abb.length; i++) {
+			let obj = {}
+			obj = {
+				id: i,
+				abb: this.abb[i],
+				rate: this.rates[i]
+			}
+			this.cleanData.push(obj);
+		}
+		// console.log(this.cleanData);
+		// console.log(this.abb);
+		// console.log(this.rates);
+		// console.log(keys);
+		// console.log(currencyRates);
+	});
+      
+  },
+  
 }
+
 </script>
 
 <style scoped>
